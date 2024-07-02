b00c54748b531d1b651d958bfa971238b78d773b
Pedro Cardoso <pmcardoso.bcl@gmail.com>
2024-07-02 17:50:43 +0100

    .

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "ox_evidence_system"
+description "OX Lib based Evidence System"
+author "petherbcl"
+version "0.0.1"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
